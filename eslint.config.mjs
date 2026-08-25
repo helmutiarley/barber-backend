c030e17a987c378bac2de1485203e550dd008af4
Helmut Iarley <helmut.silva@hostinger.com>
2026-08-25 17:51:35 -0300

    chore: revert ts to 5.9.3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ export default tseslint.config(
   {
     files: ['**/*.ts'],
     rules: {
+      '@typescript-eslint/no-namespace': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
